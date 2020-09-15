eabf807c25c928bcd1a4119713e3b085682d0699
jihaijun <jihaijun-os@360os.com>
2020-09-15 11:00:31 +0800

    fix issue

--- manim2/__init__.py
+++ manim2/__init__.py
@@ -5,7 +5,7 @@ import manim2.extract_scene
 
 
 def main():
-    args = manimlib.config.parse_cli()
-    config = manimlib.config.get_configuration(args)
-    manimlib.constants.initialize_directories(config)
-    manimlib.extract_scene.main(config)
+    args = manim2.config.parse_cli()
+    config = manim2.config.get_configuration(args)
+    manim2.constants.initialize_directories(config)
+    manim2.extract_scene.main(config)
